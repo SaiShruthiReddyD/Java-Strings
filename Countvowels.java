ee8e34f03a78a52cf20a1b822835fc1553befaea
SaiShruthiReddyD <saishruthidhesham@gmail.com>
2026-07-21 19:45:59 +0530

    Add files via upload

---
+++ Countvowels.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Countvowels {
+    public static void main(String args[]){
+       Scanner sc=new Scanner(System.in); 
+       String s=sc.nextLine();
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'||s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U'){
+                count++;
+            }
+        }System.out.println(count);
+    }
+}
