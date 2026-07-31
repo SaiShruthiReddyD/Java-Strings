ae47d755b2f78d4c65d5e98224fc3b4a43ef2ffe
SaiShruthiReddyD <saishruthidhesham@gmail.com>
2026-07-31 19:38:21 +0530

    Add files via upload

---
+++ Stringbuilder.java
@@ -0,0 +1,11 @@
+public class Stringbuilder {
+    public static void main(String args[]){
+        StringBuilder str=new StringBuilder("Hello");
+        System.out.println(str);
+        System.out.println(str.append("123"));
+        System.out.println(str);
+        str.insert(5,"abc");
+        System.out.println(str);
+        
+    }
+}
